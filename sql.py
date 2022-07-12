39155741ee39492559cce10fa8cb191960aa953a
sayak9495 <sayak1997sen@gmail.com>
2022-07-13 01:18:28 +0530

    fixed mysql reconnection error - https://stackoverflow.com/questions/27537892/cursor-raise-errors-operationalerrormysql-connection-not-available-operat

--- sql.py
+++ sql.py
@@ -10,6 +10,7 @@ mydb = mysql.connector.connect(
 )
 
 def get_ambulance_data():
+  mydb.reconnect()
   mycursor = mydb.cursor()
   mycursor.execute("SELECT * FROM med_alert_ambulance_details;")
   myresult = mycursor.fetchall()
@@ -18,6 +19,7 @@ def get_ambulance_data():
   return myresult
 
 def update_ambulance_state(state,vehicle_number):
+  mydb.reconnect()
   mycursor = mydb.cursor()
   sql = "UPDATE med_alert_ambulance_details SET state = '"+state+"' WHERE vehicle_number = '"+vehicle_number+"'"
   mycursor.execute(sql)
@@ -25,6 +27,7 @@ def update_ambulance_state(state,vehicle_number):
   print("[DB TOUCH] updated status of "+vehicle_number+" to "+state)
 
 def get_available_ambulance():
+  mydb.reconnect()
   mycursor = mydb.cursor()
   mycursor.execute("select * from med_alert_ambulance_details where state='available';")
   myresult = mycursor.fetchall()
